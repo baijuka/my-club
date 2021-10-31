3e9ad18c4019745e6a95bff7cf9c58822b15d7be
baijuka <baijuka@yahoo.com>
2021-10-31 20:30:58 +0000

    Add calendar in view

--- myclub_site/events/views.py
+++ myclub_site/events/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import date
+import calendar
+from calendar import HTMLCalendar
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("<h1>My Club Event Calendar</h1>")
+def index(request, year, month):
+    month = int(month)
+    year = int(year)
+    if year < 2000 or year > 2099: year = date.today().year
+    month_name = calendar.month_name[month]
+    title = "MyClub Event Calendar - %s %s" % (month_name, year)
+    cal = HTMLCalendar().formatmonth(year,month)
+    return HttpResponse("<h1>%s</h1><p>%s</p>" % (title,cal))
